4bf296e858c8d48d805c27ce43809d7267225032
KorkeshkoAlex <KorkeshkoAlex@mail.ru>
2018-06-27 17:57:55 +0300

    Добавьте файлы проекта.

---
+++ OSISP_Task_9/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace OSISP_Task_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelText = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.labelText2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(12, 40);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(0, 13);
+            this.labelText.TabIndex = 0;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(12, 75);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // labelText2
+            // 
+            this.labelText2.AutoSize = true;
+            this.labelText2.Location = new System.Drawing.Point(12, 9);
+            this.labelText2.Name = "labelText2";
+            this.labelText2.Size = new System.Drawing.Size(0, 13);
+            this.labelText2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 108);
+            this.Controls.Add(this.labelText2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.labelText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label labelText2;
+    }
+}
+
---
+++ OSISP_Task_9/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OSISP_Task_9
+{
+    public partial class Form1 : Form
+    {
+        private int i = 0;
+        private int ii = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Method1()
+        {
+            Thread.Sleep(1000);
+            this.BeginInvoke(new Action(delegate() { this.labelText.Text = "Обновлён " + (i++); }));
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            Thread tread = new Thread(Method1);         
+            tread.Start();
+            this.labelText2.Text = "Запись " + (ii++);
+        }
+    }
+}
